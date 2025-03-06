05ef9bcd40dbedfc2834d6263c53ac255c980d3f
azam.feyznia@gmail.com <azam.feyznia@gmail.com>
2025-03-06 21:01:20 +0330

    feat: Add Article model and migration

---
+++ app/Models/Article.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'articles';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'content',
+        'url',
+        'source',
+        'category',
+        'published_at',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'published_at' => 'datetime',  // To treat published_at as a Carbon instance
+    ];
+
+}
