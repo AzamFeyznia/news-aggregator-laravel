bafc527369917d91bd6aa7f102662f222a31c850
azam.feyznia@gmail.com <azam.feyznia@gmail.com>
2025-03-07 00:02:16 +0330

    feat: Implement API rate limiting to protect endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,7 @@
 use App\Http\Controllers\Api\ArticleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/articles', [ArticleController::class, 'index']);
-Route::get('/articles/{id}', [ArticleController::class, 'show']);
+Route::middleware('throttle:api')->group(function () {
+    Route::get('/articles', [ArticleController::class, 'index']);
+    Route::get('/articles/{id}', [ArticleController::class, 'show']);
+});
